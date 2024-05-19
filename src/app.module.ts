d3d596395a668f537208ea401aa10fe68a5d6f0f
Igor J. Santos <igor@igorjs.io>
2024-05-19 17:22:14 +1000

    Update db schema and create data service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { SchedulesModule } from './schedules/schedules.module';
 import { TasksModule } from './tasks/tasks.module';
+import { DataService } from './data/data.service';
 
 @Module({
   imports: [SchedulesModule, TasksModule],
-  providers: [],
+  providers: [DataService],
   controllers: [AppController],
 })
 export class AppModule {}
