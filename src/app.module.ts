21b757969fa3fd7020fc54af1cc36042d1f079b3
Igor J. Santos <igor@igorjs.io>
2024-05-19 17:54:33 +1000

    Enable prisma logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { DataService } from './data/data.service';
 import { ScheduleModule } from './schedule/schedule.module';
 import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [ScheduleModule, TaskModule],
-  providers: [DataService],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DataService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+    });
+  }
   async onModuleInit() {
+    this.$on('query' as never, async (e: Prisma.QueryEvent) => {
+      console.log('Query: ' + e.query);
+      console.log('Params: ' + e.params);
+      console.log('Duration: ' + e.duration + 'ms');
+    });
     await this.$connect();
   }
 }
