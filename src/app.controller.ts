a1d36153f8a8e03ba019881a53c1278e8880cdcc
Igor J. Santos <igor@igorjs.io>
2024-05-19 16:54:41 +1000

    Switch http server to Fastify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Header, HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Header('Cache-Control', 'no-cache')
+  @HttpCode(HttpStatus.OK)
+  index() {
+    return 'OK';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import compression from '@fastify/compress';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.register(compression);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
